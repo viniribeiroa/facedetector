415db1446983cb9312f8d5342c9edb59ebfe8fa8
Vinicius Alves Ribeiro <viniribeiroa@gmail.com>
2022-09-09 16:44:42 -0300

    Paramentros de face

--- src/main/java/com/viniciusribeiro/facedetector/Captura.java
+++ src/main/java/com/viniciusribeiro/facedetector/Captura.java
@@ -1,13 +1,23 @@
 package com.viniciusribeiro.facedetector;
 
 
+import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGRA2GRAY;
+import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
+
 import java.awt.event.KeyEvent;
 
 import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber.Exception;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
-import org.bytedeco.javacv.Frame;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Rect;
+import org.bytedeco.opencv.opencv_core.RectVector;
+import org.bytedeco.opencv.opencv_core.Scalar;
+import org.bytedeco.opencv.opencv_core.Size;
+import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
+import static org.bytedeco.opencv.global.opencv_imgproc.rectangle;
 
 public class Captura {
 	
@@ -19,10 +29,26 @@ public class Captura {
 		OpenCVFrameGrabber camera = new OpenCVFrameGrabber(0);
 		camera.start();
 		
+		CascadeClassifier detectorFace = new CascadeClassifier("src\\main\\java\\com\\viniciusribeiro\\facedetector\\resource\\haarcascade_frontalface_alt.xml");
+		
 		CanvasFrame cFrame = new CanvasFrame("Preview", CanvasFrame.getDefaultGamma()/ camera.getGamma());
 		Frame frameCapturado = null;
+		Mat imagemColorida = new Mat();
+		
 		while((frameCapturado = camera.grab()) != null) {
 			
+			imagemColorida = converteMat.convert(frameCapturado);
+			Mat imagemCinza = new Mat();
+			cvtColor(imagemColorida, imagemCinza, COLOR_BGRA2GRAY);
+			
+			RectVector facesDetectadas = new RectVector();
+			detectorFace.detectMultiScale(imagemCinza, facesDetectadas, 1.1, 1, 0, new Size(150, 150), new Size(500, 500));
+			
+			for(int i =0; i < facesDetectadas.size(); i++) {
+				
+				Rect dadosFace = facesDetectadas.get(0);
+				rectangle(imagemColorida, dadosFace, new Scalar(0,0,255,0));
+			}
 			if(cFrame.isVisible()) {
 				cFrame.showImage(frameCapturado);
 			}
